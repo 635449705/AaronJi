44f864f7e295c7ec3b660ee873bf5bb150502901
635449705 <46984044+635449705@users.noreply.github.com>
2019-03-22 10:31:50 -0500

    this is studentfile

---
+++ student sample/Chap10/Car Truck SUV Demo/Car Truck SUV Demo/Truck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Truck_SUV_Demo
+{
+    class Truck : Automobile
+    {
+        // Field
+        private string _drive;
+
+        // Constructor
+        public Truck()
+        {
+            _drive = "";
+        }
+
+        // Drive property
+        public string Drive
+        {
+            get { return _drive; }
+            set { _drive = value; }
+        }
+    }
+}
